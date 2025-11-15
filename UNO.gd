91dc298b354de784ab88f9df25ba3ee661c4ea5d
parking_turkeys <p-t@hackclub.app>
2025-11-15 18:55:34 -0500

    commit

--- UNO.gd
+++ UNO.gd
@@ -16,6 +16,7 @@ func _ready() -> void:
 	deal_cards();
 	start_discard();
 	show_discard();
+	setup_hand();
 	pass # Replace with function body.
 
 
@@ -32,17 +33,17 @@ func create_deck():
 		new_card.colour = colour_codes[i % 8 / 2];
 		new_card.number = str(floor(i/8)+ 1);
 		globals.deck.append(new_card);
-	print(globals.deck);
+	# print(globals.deck);
 
 func deal_cards():
 	for i in range(HANDSIZE*2):
 		match i % 2:
 			0:
 				globals.hand.append(globals.deck[0]);
-				globals.deck.erase(0);
+				globals.deck.erase(globals.deck[0]);
 			1:
 				globals.opp_hand.append(globals.deck[0]);
-				globals.deck.erase(0);
+				globals.deck.erase(globals.deck[0]);
 
 func start_discard():
 	globals.discard.append(globals.deck[0]);
@@ -57,4 +58,26 @@ func show_discard():
 
 
 func _on_card_submit() -> void:
-	pass
+	var submitted_card = globals.hand[get_node("%Hand_Options").selected]
+	if (submitted_card.colour == globals.discard[globals.discard.size() - 1].colour):
+		pass
+	elif (submitted_card.number == globals.discard[globals.discard.size() - 1].number):
+		pass
+	
+
+func setup_hand():
+	print(globals.hand);
+	for i in globals.hand:
+		# turn colour to string
+		var colour_str = ""
+		match i.colour:
+			Color("RED"):
+				colour_str = "Red"
+			Color("GREEN"):
+				colour_str = "Green"
+			Color("BLUE"):
+				colour_str = "Blue"
+			Color("YELLOW"):
+				colour_str = "Yellow"
+		get_node("%Hand_Options").add_item(colour_str + " " + i.number);
+		
