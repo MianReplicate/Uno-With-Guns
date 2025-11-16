fee0d9a504fc3d11bc8754c07d2203d8bda8433a
vivithequeen <vivithequeen1@gmail.com>
2025-11-16 02:32:36 -0400

    meowwww

--- uno/UnoGame.cs
+++ uno/UnoGame.cs
@@ -8,44 +8,45 @@ using System.Threading.Tasks;
 public partial class UnoGame : Node
 {
 	public System.Collections.Generic.Dictionary<string, Color> ColourCodes =
-        new()
-        {
-            {"RED", new Color(255, 0, 0)},
+		new()
+		{
+			{"RED", new Color(255, 0, 0)},
 			{"GREEN", new Color(0, 255, 0)},
 			{"YELLOW", new Color(255, 255, 0)},
 			{"BLUE", new Color(0, 0, 255)},
-        };
-	
+		};
+
 	const int HANDSIZE = 7;
 	public List<Card> discard;
 	public List<Card> deck;
 	public List<Card> hand;
 	public static UnoGame Instance;
-    public override async void _Ready()
-    {
-		
-        StartIfNotStarted();
-    }
+	public override async void _Ready()
+	{
+
+		StartIfNotStarted();
+	}
 
 	private void StartIfNotStarted()
-    {
-        if(Instance == null){
+	{
+		if (Instance == null)
+		{
 			Instance = this;
 
-            if (Networking.Instance.IsHost)
-            {
+			if (Networking.Instance.IsHost)
+			{
 				hand = new List<Card>();
 				discard = new List<Card>();
 				CreateDeck();
 				StartDiscard();
-            }
+			}
 		}
 		return;
-    }
+	}
 
-	public void DrawCard(bool forClient=false)
-    {
-        if(!Networking.Instance.IsHost)
+	public void DrawCard(bool forClient = false)
+	{
+		if (!Networking.Instance.IsHost)
 			return;
 
 		hand.Add(deck[0]);
@@ -54,63 +55,103 @@ public partial class UnoGame : Node
 		RefreshHand();
 	}
 	public Card GetTopMostCardFromList(List<Card> cards)
-    {
-        return cards[cards.Count - 1];
-    }
+	{
+		return cards[cards.Count - 1];
+	}
 	public void RefreshHand()
-    {
+	{
 		var options = ((OptionButton)GetNode("%Hand_Options"));
 		options.Clear();
 
-		foreach(var card in hand)
-        {
+		foreach (var card in hand)
+		{
 			var colour_str = card.colourName;
 
 			options.AddItem(colour_str + " " + card.number);
-        }
-    }
+		}
+	}
 	public void StartDiscard()
-    {
+	{
 		Discard(0);
-    }
+	}
 	public void Discard(int index)
-    {
-        discard.Add(deck[index]);
+	{
+		discard.Add(deck[index]);
 		deck.RemoveAt(index);
 
 		var latestDiscardCard = GetTopMostCardFromList(discard);
 		GetNode("%Colour").Set("Color", latestDiscardCard.colour);
 		GetNode("%Number").Set("Text", latestDiscardCard.number);
-    }
+	}
 	public void CreateDeck()
-    {
+	{
 		var generator = new RandomNumberGenerator();
 
 		var tempCards = new List<Card>();
 		// 9 colors
 		// 8 (2 for each color)
 		var max = 9 * 8;
-		
+
 		var keys = ColourCodes.Keys.ToList();
-		for(int i = 0; i < max; i++)
-        {
+		for (int i = 0; i < max; i++)
+		{
 			ColourCodes.TryGetValue(keys[i % 8 / 2], out var color);
-			var new_card = new Card(color, keys[i% 8 / 2], (Math.Floor(i/8.0)+ 1).ToString());
+			var new_card = new Card(color, keys[i % 8 / 2], (Math.Floor(i / 8.0) + 1).ToString());
 			tempCards.Add(new_card);
-        }
+		}
 
 		deck = new List<Card>();
-		for(int i = 0; i < tempCards.Count; i++)
-        {
+		for (int i = 0; i < tempCards.Count; i++)
+		{
 			var index = generator.RandiRange(0, tempCards.Count - 1);
 			var card = tempCards[index];
 			tempCards.RemoveAt(index);
-			deck.Add(card);      
+			deck.Add(card);
+		}
+
+		for (int i = 0; i < HANDSIZE * 2; i++)
+		{
+			DrawCard(i % 2 == 0 ? true : false);
+		}
+	}
+	public void _on_card_submit()
+	{
+		Card submitted_card = hand[GetNode<OptionButton>("%Hand_Options").Selected];
+
+		if(submitted_card.colour == discard[discard.Count - 1].colour)
+        {
+            Discard(GetNode<OptionButton>("%Hand_Options").Selected);
+
+			GetNode<Label>("%Notice").Text = "";
+        }
+		else if(submitted_card.number == discard[discard.Count - 1].number)
+        {
+            Discard(GetNode<OptionButton>("%Hand_Options").Selected);
+
+			GetNode<Label>("%Notice").Text = "";
+        }
+        else
+        {
+            GetNode<Label>("%Notice").Text = "Frick u";
         }
 
-		for(int i = 0; i < HANDSIZE * 2; i++)
-        {
-			DrawCard(i % 2 == 0 ? true: false);
-        }
-    }
+	}
+
+
+	// func _on_card_submit() -> void:
+	// 	var submitted_card = globals.hand[get_node("%Hand_Options").selected]
+	// 	#print(globals.discard[globals.discard.size() - 1].colour)
+	// 	#print(submitted_card.colour)
+	// 	if (submitted_card.colour == globals.discard[globals.discard.size() - 1].colour):
+	// 		discard_card(get_node("%Hand_Options").selected);
+	// 		%Notice.set_text("");
+	// 	elif (submitted_card.number == globals.discard[globals.discard.size() - 1].number):
+	// 		discard_card(get_node("%Hand_Options").selected)
+
+	// 		%Notice.set_text("");
+	// 	else:
+	// 		%Notice.set_text("fuck no");
+
+
+	// 	post_turn();
 }
